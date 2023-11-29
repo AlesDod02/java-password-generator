ecc180ec439e34212894148f4c3d2d62106f716c
AlesDod02 <alessandro.dod@gmail.com>
2023-11-29 14:26:24 +0100

    final commit

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,33 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("put name");
+        String name = scan.next();
+        System.out.println("put surname");
+        String surname = scan.next();
+        System.out.println("put favorite color");
+        String favoriteColor = scan.next();
+        System.out.println("put your day of birth");
+        int day = scan.nextInt();
+        System.out.println("put your month of birth");
+        int month = scan.nextInt();
+        System.out.println("put your year of birth");
+        int year = scan.nextInt();
+        int sum = day + month + year;
+        System.out.println("your password is"+" "+name+"-"+ surname+"-"+favoriteColor+"-"+sum);
+
+
+
+
+
+
+
+
+        scan.close();
 
     }
 }
